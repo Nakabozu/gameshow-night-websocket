925d73c4a4cd3e0163a9d96d08a4682d8754ac66
Naka Freitag <spamnaka@gmail.com>
2025-12-16 19:49:24 -0500

    Allowing all incoming traffic

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const io = new Server(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
         };
         res.writeHead(200, headers);
